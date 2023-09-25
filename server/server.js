314841e1cbf9762aec8c595c9b73216e65364c0b
nikhilscaria3 <125331845+nikhilscaria3@users.noreply.github.com>
2023-09-25 17:39:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
 
 
 app.use(cors({
-  origin: 'http://localhost:5000',
+  origin: 'http://localhost:3000',
   credentials: true, // If you need to send cookies or credentials
 }));
 
