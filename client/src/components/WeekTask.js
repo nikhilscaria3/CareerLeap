b9efe4e78d9fefdaf235cf61da08f85cfdc4e220
nikhilscaria3 <125331845+nikhilscaria3@users.noreply.github.com>
2023-10-09 23:13:04 +0530

    Bug Fix

--- client/src/components/WeekTask.js
+++ client/src/components/WeekTask.js
@@ -1,121 +1,71 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/weektask.css';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { WeekTaskInfo } from './WeekTaskInfo';
-import axios from 'axios';
-import Circular from '../views/Ciricular.jpg'
+import Circular from '../views/Ciricular.jpg';
 import careerleaplogo from '../views/careerleaplogo2.png';
+import axios from 'axios';
+import { setAuthToken } from '../utils/api';
 
 
 export function WeekTask() {
-  const [week, setWeek] = useState(null);
-  const [weekNo, setWeekNumber] = useState(null);
-  const [daysRemaining, setDaysRemaining] = useState(0);
-  const navigate = useNavigate();
+  const [week, setWeek] = useState('');
   const [weekTasks, setWeekTasks] = useState([]);
-  const encodedData = localStorage.getItem("randomsession");
+  const [loading, setLoading] = useState(true);
   const [showImage, setShowImage] = useState(false);
-
-  // Step 2: Decode the data using Base64
+  const navigate = useNavigate();
+  const encodedData = localStorage.getItem("randomsession");
   const decodedData = atob(encodedData);
-
-  // Step 3: Extract the original loginUserData f const timestamp = decodedData.substring(0, 13); // Assuming the timestamp is 13 digits
-  const jwtemail = decodedData.substring(13); // Extracting data after the timestamp
-
-  const [email, setemail] = useState(null)
-
-  console.log("setdata", weekTasks);
+  const jwtemail = decodedData.substring(13);
 
   useEffect(() => {
-    const fetchTimerDataFromBackend = async () => {
-      try {
-        const response = await axios.get("/api/getTimerData");
-        setDaysRemaining(response.data.remainingDays);
-        console.log("Timer data fetched from the backend!");
-      } catch (error) {
-        console.error("API Call Error:", error);
-      }
-    };
+    const fetchData = async () => {
+      const token = localStorage.getItem('jwtLoginToken');
+            try {
+                setAuthToken(token);
+        // Fetch task data
+        const taskResponse = await axios.get("/gettaskinfoweek");
+        const taskInfoData = taskResponse.data.data;
 
-    fetchTimerDataFromBackend();
-  }, []);
-
-  useEffect(() => {
-    // Check for authentication
-    const isAuthenticated = !!localStorage.getItem("jwtLoginToken");
-    if (!isAuthenticated) {
-      // If not authenticated, redirect to the login page
-      navigate("/login");
-    }
-  }, [navigate]);
-
-  useEffect(() => {
-    const userdata = async () => {
-      try {
-        const response = await axios.get(`/getuserdata/${jwtemail}`, {
-          params: { jwtemail }
-        });
-
-        console.log("data gett", response.data);
-        setemail(response.data.useremail)
-        setWeek(response.data.userweek)
-        // Do something with the data here, e.g., set it to a state variable
-        // Example: setUserData(data);
-      } catch (error) {
-        // Handle any errors that occurred during the request
-        console.log(error);
-      }
-    };
-
-    userdata();
-  }, []); // Add the dependency here if you need to refetch when the email changes
-
-
-
-  useEffect(() => {
-    const fetchTaskInfo = async () => {
-      try {
-        const response = await axios.get("/gettaskinfoweek");
-        const taskInfoData = response.data.data; // Access the 'data' property here
-        console.log("taskInfoData:", taskInfoData);
-        console.log("Type of taskInfoData:", typeof taskInfoData);
-
-        // Check if taskInfoData is an array before setting it to weekTasks
         if (Array.isArray(taskInfoData)) {
           setWeekTasks(taskInfoData);
         } else {
           console.error("Task data is not an array:", taskInfoData);
         }
+
+        // Fetch user data
+        const userResponse = await axios.get(`/getuserdata/${jwtemail}`, {
+          params: { jwtemail }
+        });
+        const userData = userResponse.data;
+        setWeek(userData.userweek || ""); // Set default value as an empty string if userweek is null or undefined
       } catch (error) {
         console.error("API Call Error:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchTaskInfo();
-  }, []);
-
+    fetchData();
+  }, [jwtemail]);
 
   const showWeek = (weekNumber) => {
-    setWeek(`Week ${weekNumber}`);
-    setWeekNumber(`${weekNumber}`);
     navigate('/weektaskinfo', { state: { data: `Week ${weekNumber}`, weeknumber: weekNumber } });
   };
 
   const handlenavigate = () => {
-    navigate('/profile')
-  }
+    navigate('/profile');
+  };
 
   const handlemanifestnavigate = () => {
     navigate("/manifest", { state: { manifestemail: jwtemail } });
   };
 
   const handlecoursenavigate = () => {
-    navigate('/courses')
-  }
-
+    navigate('/courses');
+  };
 
   const handleToggleImage = () => {
     // Toggle the state of showImage between true and false
@@ -126,66 +76,53 @@ export function WeekTask() {
     <div>
       <nav className="navbar">
         <div className="navbrand">
-        <img className="navlogo" src={careerleaplogo} alt="Form Logo" />
+          <img className="navlogo" src={careerleaplogo} alt="Form Logo" />
         </div>
         <div className='coursecategory'>
           <ul>
             <button onClick={handlemanifestnavigate}>Manifest</button>
             <button onClick={handlecoursenavigate}>Courses</button>
           </ul>
-
         </div>
-
-
         <div className="adminloginbutton">
           <button onClick={handlenavigate}>
             <FontAwesomeIcon icon={faUser} />
           </button>
         </div>
       </nav>
-
       <div className="weektaskcontainer">
         <h1>28 Week Journey</h1>
-        <p></p>
+       
         <div className="weekbuttons">
-          {weekTasks.map((data, index) => {
-            const weekNumber = index + 1;
-            const isDisabled = weekNumber > parseInt(week.slice(-1));
-
-            return (
-              <button
-                key={weekNumber}
-                onClick={() => showWeek(weekNumber)}
-                disabled={isDisabled}
-                className={isDisabled ? 'disabled-button' : ''}
-              >
-                {isDisabled ? <span className="lock-icon">🔒</span> : null}
-                Week {weekNumber}
-                {/* Debugging information */}
-                {/* <span>status: {JSON.parse(data.status) ? 'Locked' : 'Unlocked'}</span> */}
-              </button>
-            );
-          })}
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            weekTasks.map((data, index) => {
+              const weekNumber = index + 1;
+              const isDisabled = weekNumber > parseInt(week.slice(-1), 10);
+              return (
+                <button
+                  key={weekNumber}
+                  onClick={() => showWeek(weekNumber)}
+                  disabled={isDisabled}
+                  className={isDisabled ? 'disabled-button' : ''}
+                >
+                  {isDisabled ? <span className="lock-icon">🔒</span> : null}
+                  Week {weekNumber}
+                </button>
+              );
+            })
+          )}
         </div>
-
-
-
-
       </div>
-
       <div className="weektaskcontainer">
-      <div>
-      {/* Step 4: Add the button and the event handler */}
-      <button className='circularbutton' onClick={handleToggleImage}>
-        {showImage ? 'Hide Note' : 'Show Note'}
-      </button>
-
-      {/* Step 5: Conditionally render the image */}
-      {showImage && <img className='circularimage' src={Circular} alt="Your Image" />}
-    </div>
+        <div>
+          <button className='circularbutton' onClick={handleToggleImage}>
+            {showImage ? 'Hide Note' : 'Show Note'}
+          </button>
+          {showImage && <img className='circularimage' src={Circular} alt="Your Image" />}
+        </div>
       </div>
-
     </div>
-
   );
 }
