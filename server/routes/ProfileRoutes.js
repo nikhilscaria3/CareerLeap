c7c2ea60ddd5763aff474ae37e79f9e10eb28c64
nikhilscaria3 <125331845+nikhilscaria3@users.noreply.github.com>
2023-10-10 17:40:12 +0530

    Update ProfileRoutes.js

--- server/routes/ProfileRoutes.js
+++ server/routes/ProfileRoutes.js
@@ -23,7 +23,9 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
- 
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB file size limit (adjust as needed)
+  },
 });
 
 
